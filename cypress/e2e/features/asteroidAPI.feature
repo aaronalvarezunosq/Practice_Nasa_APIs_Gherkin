7b8d54528162afdc257d900f46504a60f44ceaa0
aaronalvarezunosq <128838330+aaronalvarezunosq@users.noreply.github.com>
2023-04-05 14:21:26 -0600

    implemented gherkin steps for first API test

---
+++ cypress/e2e/features/asteroidAPI.feature
@@ -0,0 +1,9 @@
+Feature: Testing GET requests to NASA asteroid API
+
+Scenario: Perform GET request to asteroid API and validate response body
+
+Given I set up the query parameters to send to the API URL
+
+When I make the request to the API
+
+Then I perform multiple validations on the response body
---
+++ cypress/stepDefinitions/asteroidAPIStepDefinition.js
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+import {Given, When, Then} from '@badeball/cypress-cucumber-preprocessor';
+const asteroids = require('../support/pageObject/testData/asteroidsAPI.json');
+const apiKey = require('../support/pageObject/testData/api_key.json');
+let baseURL;
+
+
+Given('I set up the query parameters to send to the API URL', () => {
+    baseURL = 'https://api.nasa.gov/neo/rest/v1/feed'
+})
+
+When('I make the request to the API', () => {
+    cy.request({
+        method: 'GET',
+        url: baseURL,
+        qs:{"start_date":asteroids.start_date,"end_date":asteroids.end_date,"api_key":apiKey.api_key} //passing parameters for the request
+      }).as('request');
+})
+
+Then('I perform multiple validations on the response body', () => {
+    cy.get('@request').then((response) =>{
+        //validamos el body de la respuesta
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.property('element_count')
+        expect(response.body).to.have.property('links')
+    })        
+})
